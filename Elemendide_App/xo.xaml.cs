7a7ec235547d5d849fadbaab7a664d885a9f862d
opilane <opilane@HP-CZC6297B98>
2022-03-08 12:29:59 +0200

    śdfsdfsfsdf

--- Elemendide_App/xo.xaml.cs
+++ Elemendide_App/xo.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,9 +12,8 @@ namespace Elemendide_App
     public partial class xo : ContentPage
     {
         Grid grid2X1, grid3X3;
-        BoxView b;
+        Image b;
         Button uus_mang;
-        Image x, o;
         public bool esimene;
         int tulemus = -1;
         int[,] Tulemused = new int[3, 3];
@@ -49,7 +47,7 @@ namespace Elemendide_App
         }
         public async void Kes_on_esimene()
         {
-            string esimene_valik = await DisplayPromptAsync("Kes on esimene?", "Tee valiku Kollane-1 või Punane-2", initialValue: "1", maxLength: 1, keyboard: Keyboard.Numeric);
+            string esimene_valik = await DisplayPromptAsync("Kes on esimene?", "Tee valiku X-1 või 0-2", initialValue: "1", maxLength: 1, keyboard: Keyboard.Numeric);
             if (esimene_valik == "1")
             {
                 esimene = true;
@@ -66,11 +64,11 @@ namespace Elemendide_App
 
         public async void Uus_mang()
         {
-            bool uus = await DisplayAlert("Uus mäng", "Kas tõesti tahad uus mäng?", "Tahan küll!", "Ei taha!");
+            bool uus = await DisplayAlert("Uus mäng", "Kas tahad uus mäng?", "Jah!", "Ei!");
             if (uus)
             {
                 Kes_on_esimene();
-                int[,] Tulemused = new int[3, 3];
+                Tulemused = new int[3, 3];
                 tulemus = -1;
                 grid3X3 = new Grid
                 {
@@ -94,7 +92,7 @@ namespace Elemendide_App
                 {
                     for (int j = 0; j < 3; j++)
                     {
-                        b = new BoxView { BackgroundColor = Color.Orange };
+                        b = new Image { Source="fon.jpg" };
                         grid3X3.Children.Add(b, j, i);
                         TapGestureRecognizer tap = new TapGestureRecognizer();
                         tap.Tapped += Tap_Tapped;
@@ -105,12 +103,7 @@ namespace Elemendide_App
             }
 
         }
-        // "00""10""20", 
-        // "01""11""21", 
-        // "02""12""22",
-        // "00""01""02",
-        // "10""11""12",
-        // "20""21""22", "001122", "021120" };
+       
 
         public int Kontroll()
         {
@@ -128,21 +121,22 @@ namespace Elemendide_App
                 tulemus = 1;
             }
             //teine inimene
-            else if (Tulemused[0, 0] == 0 && Tulemused[1, 0] == 0 && Tulemused[2, 0] == 0 || Tulemused[0, 1] == 0 && Tulemused[1, 1] == 0 && Tulemused[2, 1] == 0 || Tulemused[0, 2] == 0 && Tulemused[1, 2] == 0 && Tulemused[2, 2] == 0)
+            else if (Tulemused[0, 0] == 2 && Tulemused[1, 0] == 2 && Tulemused[2, 0] == 2 || Tulemused[0, 1] == 2 && Tulemused[1, 1] == 2 && Tulemused[2, 1] == 2 || Tulemused[0, 2] == 2 && Tulemused[1, 2] == 2 && Tulemused[2, 2] == 2)
             {
-                tulemus = 0;
+                tulemus = 2;
             }
-            else if (Tulemused[0, 0] == 0 && Tulemused[0, 1] == 0 && Tulemused[0, 2] == 0 || Tulemused[1, 0] == 0 && Tulemused[1, 1] == 0 && Tulemused[1, 2] == 0 || Tulemused[2, 0] == 0 && Tulemused[2, 1] == 0 && Tulemused[2, 2] == 0)
+            else if (Tulemused[0, 0] == 2 && Tulemused[0, 1] == 2 && Tulemused[0, 2] == 2 || Tulemused[1, 0] == 2 && Tulemused[1, 1] == 2 && Tulemused[1, 2] == 2 || Tulemused[2, 0] == 2 && Tulemused[2, 1] == 2 && Tulemused[2, 2] == 2)
             {
-                tulemus = 0;
+                tulemus = 2;
             }
-            else if (Tulemused[0, 0] == 0 && Tulemused[1, 1] == 0 && Tulemused[2, 2] == 0 || Tulemused[0, 2] == 0 && Tulemused[1, 1] == 0 && Tulemused[2, 0] == 0)
+            else if (Tulemused[0, 0] == 2 && Tulemused[1, 1] == 2 && Tulemused[2, 2] == 2 || Tulemused[0, 2] == 2 && Tulemused[1, 1] == 2 && Tulemused[2, 0] == 2)
             {
-                tulemus = 0;
+                tulemus = 2;
             }
+            
             else 
             {
-                tulemus = 3;
+                tulemus = -1;
             }
             return tulemus;
         }
@@ -151,34 +145,33 @@ namespace Elemendide_App
             tulemus = Kontroll();
             if (tulemus == 1)
             {
-                DisplayAlert("Võit", "Esimine on võitja!", "Ok");
+                DisplayAlert("Võit", "Sa oled võitja!", "Ok");
             }
-            else if (tulemus == 0)
+            else if (tulemus == 2)
             {
-                DisplayAlert("Võit", "Teine on võitja!", "Ok");
+                DisplayAlert("Võit", "Sinu sõber on võitja!", "Ok");
             }
-            else if(tulemus == 3)
-            {
-                DisplayAlert("Ei", "Keegi ei on võitja!", "Ok");
-            }
+            
+
+
             
         }
         private void Tap_Tapped(object sender, EventArgs e)
         {
-            var b = (BoxView)sender;
+            var b = (Image)sender;
             var r = Grid.GetRow(b);
             var c = Grid.GetColumn(b);
             if (esimene == true)
             {
-                b = new BoxView { BackgroundColor = Color.Yellow };
+                b = new Image{ Source ="x.png"  };
                 esimene = false;
                 Tulemused[r, c] = 1;
             }
             else
             {
-                b = new BoxView { BackgroundColor = Color.Red };
+                b = new Image { Source = "o.png"};
                 esimene = true;
-                Tulemused[r, c] = 0;
+                Tulemused[r, c] = 2;
             }
             grid3X3.Children.Add(b, c, r);
             Lopp();
